349ba47973e79123374cd8b62bc546a42a89e483
ankur-anand <ankur.anand12@gmail.com>
2017-10-08 00:21:32 +0530

    200 status within 20 seconds, to let the Message platform know that we successfully received the callback. Otherwise, the request will time out and we will keep trying to resend.

--- controllers/api/v1/messengerWebHook_controller.js
+++ controllers/api/v1/messengerWebHook_controller.js
@@ -1,4 +1,6 @@
 const keys = require('../../../keys/key');
+const request = require('request');
+
 module.exports = {
   getWebhook(req, res) {
     if (req.query['hub.verify_token'] === keys.VERIFY_TOKEN) {
@@ -9,6 +11,42 @@ module.exports = {
   },
 
   postWebHook(req, res) {
-    console.log(req.body);
+    const data = req.body;
+    // Making sure this is page subscription
+    // if (data.object === 'page') {
+    //   data.entry.forEach(entry => {
+    //     // iterate over each messaging event
+    //     entry.messaging.forEach(messEvent => {
+    //       const sender = messEvent.sender.id;
+    //       if (messEvent.message && messEvent.message.text) {
+    //         const text = messEvent.message.text;
+    //         sendTextMessage(sender, `roger that! ${text} from messenger bot`);
+    //       }
+    //     });
+    //   });
+    // }
+    res.status(200);
   }
 };
+
+function sendTextMessage(sender, text) {
+  let messageData = { text: text };
+  request(
+    {
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: keys.PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: {
+        recipient: { id: sender },
+        message: messageData
+      }
+    },
+    function(error, response, body) {
+      if (error) {
+        console.log('Error sending messages: ', error);
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+      }
+    }
+  );
+}
