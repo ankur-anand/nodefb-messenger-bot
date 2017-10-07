8ce6c4969ac0b5425e3af6207d7ed11f4734434f
ankur-anand <ankur.anand12@gmail.com>
2017-10-07 23:43:40 +0530

    refactored

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const keys = require('./keys/key');
+const routes = require('./routes/index');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//index route
-app.get('/', (req, res) => {
-  res.send('Hello World, I am a chat bot');
-});
-
-// for fb verification
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.verify_token'] === keys.VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');
-  }
-});
+app.use('/', routes);
 
 module.exports = app;
